8d69ff8b286353514d528bfa8b1007337bd983f9
cx9941 <cx9941@gmail.com>
2024-08-12 10:50:36 +0800

    rm result

--- benchmark/multivariate_time_series/exp/exp_long_term_forecasting.py
+++ benchmark/multivariate_time_series/exp/exp_long_term_forecasting.py
@@ -272,7 +272,7 @@ class Exp_Long_Term_Forecast(Exp_Basic):
 
         mae, mse, rmse, mape = metric(preds, trues)
         print('mse:{}, mae:{}, dtw:{}'.format(mse, mae, dtw))
-        f = open("result_long_term_forecast.txt", 'a')
+        f = open("results/result_long_term_forecast.txt", 'a')
         f.write(setting + "  \n")
         f.write('mse:{}, mae:{}, dtw:{}'.format(mse, mae, dtw))
         f.write('\n')
